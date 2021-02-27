5cb394dbf7579f17ea40792151580e6e7484f0ce
codinghare5 <57845816+codinghare5@users.noreply.github.com>
2021-02-27 20:18:11 +0000

    practical-js

--- .eleventy.js
+++ .eleventy.js
@@ -69,6 +69,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("js");
 
   /* Markdown Overrides */
   let markdownLibrary = markdownIt({
