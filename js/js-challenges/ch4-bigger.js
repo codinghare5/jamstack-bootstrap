55d58b2cd1ac77147a4a049e9c288076908da5ce
codinghare5 <57845816+codinghare5@users.noreply.github.com>
2021-03-18 00:41:44 +0000

    corrections in bigger js and added challenge 5
    reading with understanding

--- js/js-challenges/ch4-bigger.js
+++ js/js-challenges/ch4-bigger.js
@@ -1,18 +1,20 @@
-const firstNumber = 5;
-const secondNumber = 2;
-
-
-function biggerNumber(a, b) {
+// DRY = Don't Repeat Yourself
+function biggerNumber(a, b)
+{
     var message = '';
     if(a > b) {
+        message
         return a;
     } else if(a===b) {
         message += 'ERROR: Numbers are equal.';
-        console.warn(message);
+        return message;
     } else if(isNaN(a) || isNaN(b)) {
         message += 'ERROR: Try to put numbers!';
-        console.warn(message);
+        return message;
     } else return b;
 }
 
-biggerNumber(firstNumber, secondNumber);
+console.log(biggerNumber(5, 8));
+console.log(biggerNumber(6, 1));
+console.log(biggerNumber(3, 3));
+console.log(biggerNumber(7, 'w'));
